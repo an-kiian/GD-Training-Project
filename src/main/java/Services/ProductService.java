33e088b0b96c46fb09d5cd7439a732095438ea7f
Anastasiia Kiian <nastyak931@gmail.com>
2020-07-03 16:45:18 +0300

    Add Product Service

--- src/main/java/Services/ProductService.java
+++ src/main/java/Services/ProductService.java
@@ -1,4 +1,11 @@
 package Services;
 
-public interface ProductServiceInterface {
+import model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public Product getProductById(long productId);
+    public List<Product> getProductByName(String nameProduct);
+    public Product updatePrice(long idProduct, double price) ;
 }
--- src/main/java/Services/ProductServiceImpl.java
+++ src/main/java/Services/ProductServiceImpl.java
@@ -1,4 +1,25 @@
 package Services;
 
-public class ProductServiceImpl {
+import Repositories.ProductRepositoryImpl;
+import Repositories.interfaces.ProductRepository;
+import model.Product;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    ProductRepositoryImpl productRepository;
+@Override
+    public Product getProductById(long productId) {
+        return productRepository.getProfileById(productId);
+
+    }
+    @Override
+    public List<Product> getProductByName(String nameProduct) {
+        return productRepository.getProfileById(nameProduct);
+
+    }
+    public Product updatePrice(long idProduct, double price) {
+        return productRepository.updatePrice(idProduct,price);
+
+    }
 }
