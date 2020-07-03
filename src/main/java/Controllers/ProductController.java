1dff6c2bb028b0194539a608be04db042f0cd6a3
Anastasiia Kiian <nastyak931@gmail.com>
2020-07-03 16:46:10 +0300

    Update Product Controller

--- src/main/java/Controllers/ProductController.java
+++ src/main/java/Controllers/ProductController.java
@@ -1,4 +1,31 @@
 package Controllers;
 
+import Services.ProductService;
+import Services.ProductServiceImpl;
+import model.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
 public class ProductController {
+    ProductServiceImpl productService;
+    @GetMapping("/store/product/getById")
+    public Product getProductById(@RequestBody long idProduct) {
+        return productService.getProductById(idProduct);
+    }
+
+    @GetMapping("/store/product/getByName")
+    public List<Product> getProductByField(@RequestBody String nameProduct) {
+        return productService.getProductByName(nameProduct);
+    }
+
+    @PutMapping("/store/product/updatePrice")
+    public Product updatePrice(@RequestBody long idProduct, double price) {
+        return productService.updatePrice(idProduct, price);
+    }
+
+    //TODO: how to write category
+    @PostMapping("/store/product/addProduct")
+    public void addProduct(@RequestBody Product product) {
+    }
 }
