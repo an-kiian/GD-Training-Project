117f00bf9a7c7adfa45e6060dd753080d2925c21
Pavel Vlasenko <pvlasenko@griddynamics.com>
2020-07-17 13:12:26 +0300

    Revert "add Swagger UI"
    This reverts commit 11a12a01

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan("store.model")
 @EnableJpaRepositories("store.repository")
-@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
