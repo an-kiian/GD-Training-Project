b7fcd6a006dba690e603feb79d11102a8cd6b322
Pavel Vlasenko <pvlasenko@griddynamics.com>
2020-07-22 14:01:20 +0300

    Add category from properties and category controller test

--- src/main/java/store/controller/CategoryController.java
+++ src/main/java/store/controller/CategoryController.java
@@ -1,41 +1,19 @@
 package store.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import store.dto.CategoryDTO;
-import store.service.CategoryService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private CategoryService categoryService;
-
-    @Autowired
-    CategoryController(CategoryService categoryService){
-        this.categoryService = categoryService;
-    }
-
-    @GetMapping("/store/category/{id}")
-    public CategoryDTO getCategoryById(@PathVariable Long id){
-        return categoryService.getCategoryById(id);
-    }
-
-    @GetMapping("/store/category/{name}")
-    public List<CategoryDTO> getCategoryByName(@PathVariable String name){
-        return categoryService.getCategoryByName(name);
-    }
+    @Value("#{'${categories.list}'.split(',')}")
+    List<String> categories;
 
     @GetMapping("/store/category")
-    public List<CategoryDTO> getAllCategories(){
-        return categoryService.getAllCategories();
-    }
-
-    @PostMapping("/store/category")
-    @ResponseStatus(HttpStatus.CREATED)
-    public CategoryDTO addCategory(@RequestBody CategoryDTO categoryDTO){
-        return categoryService.addCategory(categoryDTO);
+    public List<String> getAll(){
+        return categories;
     }
 }
